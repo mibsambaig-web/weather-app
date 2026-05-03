58eb3cd2f8fae7ff41e32b2285989b73f7f31068
Mibsam Baig <bismillahenterprises@Mibsams-MacBook-Air.local>
2026-05-03 12:37:15 +0500

    weather app - basic fetch working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState('Karachi');
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c152f4477abef94f48846109aad5e5f1&units=metric`)
+    .then(res => res.json())
+    .then(data => setWeather(data));
+  }, [city]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Weather App</h1>
+      { weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <h3>{weather.main.temp}°C</h3>
+          <p>{weather.weather[0].description}</p>
+          <p>Humidity: {weather.main.humidity}%</p>
+          </div>
+      )}
     </div>
   );
 }
